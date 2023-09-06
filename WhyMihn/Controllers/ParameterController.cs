7898e9edaca9dd96b59bf1c5145243f4658f9bc0
luis solis <luis.solis14@gmail.com>
2023-09-05 19:13:55 -0600

    Adding parameter module to api

---
+++ WhyMihn/Controllers/ParameterController.cs
@@ -0,0 +1,59 @@
+﻿using API.Entities;
+using API.Extensions;
+using API.Interface;
+using API.Models.Base;
+using API.Models.Parameter;
+using API.Models.Password;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("parameter")]
+    [ApiController]
+    public class ParameterController : ControllerBase
+    {
+
+        public IParameterRepository parameterRepository;
+    
+    public ParameterController(IParameterRepository parameterRepository) 
+        { 
+        this.parameterRepository = parameterRepository;
+        }
+
+        [Authorize(Role.ADMIN)]
+        [Route("GetParameter/{parameterId}/{tableName}/{tenantId}/{clientId}")]
+        [HttpGet]
+        public async Task<ActionResult<Parameter>> GetParameter(string parameterId,string tableName, string tenantId, string clientId)
+        {   
+            return await this.parameterRepository.GetParameter(parameterId,tableName,tenantId,clientId);
+        }
+
+        [Authorize(Role.ADMIN)]
+        [Route("AddParameter")]
+        [HttpPost]
+        public async Task<ActionResult<Parameter>> AddParameter(AddParameterRequest addParameterRequest)
+        {
+            var result = await this.parameterRepository.AddParameter(addParameterRequest);
+            return Ok(result);
+        }
+
+        [Authorize(Role.ADMIN)]
+        [Route("UpdateParameter")]
+        [HttpPut]
+        public async Task<ActionResult<Parameter>> UpdateParameter(UpdateParameterRequest updateParameterRequest)
+        {
+            var result = await this.parameterRepository.UpdateParameter(updateParameterRequest);
+            return Ok(result);
+        }
+
+        [Authorize(Role.ADMIN)]
+        [Route("DeleteParameter/{parameterId}/{tableName}/{tenantId}/{clientId}")]
+        [HttpDelete]
+        public async Task<ActionResult<Parameter>> DeleteParameter(string parameterId, string tableName, string tenantId, string clientId)
+        {
+            var result = await this.parameterRepository.DeleteParameter(parameterId, tableName, tenantId, clientId);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ WhyMihn/Models/Parameter/AddParameterRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Models.Parameter
+{
+    public class AddParameterRequest
+    {
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+        public string ParameterId { get; set; }
+        public string TableName { get; set; }
+        public string ParameterValue { get; set; }
+
+    }
+}
