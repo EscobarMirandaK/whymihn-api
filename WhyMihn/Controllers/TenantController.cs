b1f5f775388b8301cedbc94cdf8ac87d8cda1aca
Kevin Escobar Miranda <kescobarm94@gmail.com>
2023-09-06 22:07:48 -0600

    Create endpoints for tenant management

---
+++ WhyMihn/Controllers/TenantController.cs
@@ -0,0 +1,43 @@
+﻿using API.Entities;
+using API.Extensions;
+using API.Interface;
+using API.Models.Base;
+using API.Models.Tenant;
+using API.Models.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("tenant")]
+    [ApiController]
+    public class TenantController : ControllerBase
+    {
+        private readonly ITenantRepository tenantRepository;
+
+        public TenantController(ITenantRepository tenantRepository)
+        {
+            this.tenantRepository = tenantRepository;
+        }
+
+        [Authorize(Role.ADMIN)]
+        [HttpGet]
+        public async Task<ActionResult<List<Tenant>>> Get()
+        {
+            return await this.tenantRepository.GetTenants();
+        }
+
+        [Authorize(Role.ADMIN)]
+        [HttpPost]
+        public async Task<ActionResult<Tenant>> Create([FromBody] TenantRequest tenantRequest)
+        {
+            return await this.tenantRepository.CreateOrUpdate(tenantRequest);
+        }
+
+        [Authorize(Role.ADMIN)]
+        [HttpPut]
+        public async Task<ActionResult<Tenant>> Update([FromBody] TenantRequest tenantRequest)
+        {
+            return await this.tenantRepository.CreateOrUpdate(tenantRequest);
+        }
+    }
+}
---
+++ WhyMihn/Entities/Tenant.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
+{
+    public class Tenant
+    {
+        [Key]
+        [Column("tenant_id")]
+        public string TenantId { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+    }
+}
