8458909e78dbebbb15d30649f23f4f745aa2e45a
Kevin Escobar Miranda <kescobarm94@gmail.com>
2023-10-12 22:05:12 -0600

    Add resource endpoint

---
+++ WhyMihn/Entities/Resource.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
+{
+    public class Resource
+    {
+        [Key]
+        [Column("tenant_id")]
+        public string TenantId { get; set; }
+
+        [Column("client_id")]
+        public string ClientId { get; set; }
+
+        [Column("key")]
+        public string Key { get; set; }
+
+        [Column("value")]
+        public string Value { get; set; }
+
+        [Column("language")]
+        public string Language { get; set; }
+    }
+}
---
+++ WhyMihn/Interface/IResourceRepository.cs
@@ -0,0 +1,12 @@
+﻿using API.Entities;
+using API.Models.Resource;
+
+namespace API.Interface
+{
+    public interface IResourceRepository
+    {
+        public Task<List<Resource>> GetResources(string tenantId, string clientId);
+
+        public Task<Resource> CreateOrUpdate(ResourceRequest client);
+    }
+}
